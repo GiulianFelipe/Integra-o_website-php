dd499d75d35abba41dd9d58eed22cbb550f2cf10
GiulianFelipe <51092630+GiulianFelipe@users.noreply.github.com>
2021-04-25 00:59:01 -0300

    Add files via upload

---
+++ redflix/redflix/php/cadastro.php
@@ -0,0 +1,19 @@
+<?php
+
+$nome = $_POST['nome'];
+$senha = $_POST['senha'];
+$confirmar_senha = $_POST['confirmar_senha'];
+
+echo("Nome:");
+echo($nome);
+echo("<br>");
+
+echo("Senha:");
+echo($senha);
+echo("<br>");
+
+echo("Confirmação_de_senha");
+echo($confirmar_senha);
+
+
+ ?>
